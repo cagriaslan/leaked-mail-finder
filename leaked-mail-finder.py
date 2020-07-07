b3682589a8f1639b51da60a7c955dcc6dbfe5242
cagriaslan <sybzew-wiGfa6-gajjyn>
2020-07-07 21:00:33 +0300

    Initial commit

---
+++ leaked-mail-finder.py
@@ -0,0 +1,173 @@
+from pyhunter import PyHunter
+from itertools import count
+import keys
+import argparse
+import time
+import requests
+import json
+
+R = '\033[31m'  # red
+G = '\033[32m'  # green
+C = '\033[36m'  # cyan
+W = '\033[0m'   # white
+
+
+class EmailLeaks:
+    def __init__(self, domain_name, hunter_api_key, snov_clien_id, snov_client_secret, hibp_api_key):
+        self.email_list = set()
+        self.domain_name = domain_name
+        self.breached_dict = {}  # Breach results are saved in this
+        self.paste_dict = {}  # Paste results are saved in this
+        self.pwned_dict = {}  # This is the short summary of the data which contains pwned accounts
+        self.hunter_api_key = hunter_api_key
+        self.snov_clien_id = snov_clien_id
+        self.snov_client_secret = snov_client_secret
+        self.hibp_api_key = hibp_api_key
+
+    def get_access_token(self):
+        params = {
+            'grant_type': 'client_credentials',
+            'client_id': self.snov_clien_id,
+            'client_secret': self.snov_client_secret
+        }
+
+        res = requests.post('https://api.snov.io/v1/oauth/access_token', data=params)
+        res_text = res.text.encode('ascii', 'ignore')
+
+        return json.loads(res_text)['access_token']
+
+    def domain_search_snovio(self):
+        token = self.get_access_token()
+        c = count(0)
+        flag = True
+        while flag:
+            params = {
+                'access_token': token,
+                'domain': self.domain_name,
+                'type': 'all',
+                'limit': 100,
+                'offset': 100 * next(c)
+            }
+
+            res = requests.post('https://api.snov.io/v1/get-domain-emails-with-info', data=params)
+            res = res.json()
+            print(res)
+            for each in res["emails"]:
+                self.email_list.add(each["email"])
+
+            flag = True if res["result"] > 0 else False
+            time.sleep(1)
+
+    def domain_search_hunter(self):
+        hunter = PyHunter(self.hunter_api_key)
+        i = 0
+        search = hunter.domain_search(self.domain_name, limit=100, offset=100*i)
+        while search['emails']:
+            search = hunter.domain_search(self.domain_name, limit=100, offset=100*i)
+            for item in search['emails']:
+                user_mail = item['value']
+                self.email_list.add(user_mail)
+            print(search)
+            i += 1
+
+    def check_breached_email(self):
+        """This function uses haveibeenpwned API and checks the e-mail pwned or not. Also, creates an output file with
+        pwned e-mails."""
+        api_key = self.hibp_api_key
+        header = {'hibp-api-key': api_key}
+        for mail in self.email_list:
+            user_email = mail
+            print(C + user_email)
+            print(G + '[+]' + C + ' Checking Breach status for ' + W + '{}'.format(user_email), end='')
+            rqst = requests.get('https://haveibeenpwned.com/api/v3/breachedaccount/{}'.format(user_email),
+                                headers=header, params={'truncateResponse': 'false'}, timeout=10)
+            sc = rqst.status_code
+
+            if sc == 200:
+                print(G + ' [ pwned ]' + W)
+                json_out = rqst.content.decode('utf-8', 'ignore')
+                simple_out = json.loads(json_out)
+                self.breached_dict[user_email] = simple_out
+                self.pwned_dict[user_email] = "pwned"
+                for item in simple_out:
+                    print('\n'
+                          + G + '[+]' + C + ' Breach      : ' + W + str(item['Title']) + '\n'
+                          + G + '[+]' + C + ' Domain      : ' + W + str(item['Domain']) + '\n'
+                          + G + '[+]' + C + ' Date        : ' + W + str(item['BreachDate']) + '\n'
+                          + G + '[+]' + C + ' Fabricated  : ' + W + str(item['IsFabricated']) + '\n'
+                          + G + '[+]' + C + ' Verified    : ' + W + str(item['IsVerified']) + '\n'
+                          + G + '[+]' + C + ' Retired     : ' + W + str(item['IsRetired']) + '\n'
+                          + G + '[+]' + C + ' Spam        : ' + W + str(item['IsSpamList']))
+            elif sc == 404:
+                print(R + ' [ Not Breached ]' + W)
+            else:
+                print('\n')
+                print(R + '[-]' + C + ' An Unknown Error Occurred')
+                print(rqst.text)
+            time.sleep(1.5)  # For rate limit of haveibeenpwned API.
+        self.save_pwned()
+        self.save_all()
+
+    def check_pwned_paste(self):
+        """This function uses haveibeenpwned API and performs paste search for e-mail addresses. Creates an output file
+        with pasted accounts at the end."""
+        api_key = self.hibp_api_key
+        header = {'hibp-api-key': api_key}
+        for mail in self.email_list:
+            print(G + '[+]' + C + ' Checking Paste status for ' + W + '{}'.format(mail), end='')
+            rqst_paste = requests.get('https://haveibeenpwned.com/api/v3/pasteaccount/{}'.format(mail),
+                                      headers=header, params={'truncateResponse': 'false'}, timeout=10)
+            sc_paste = rqst_paste.status_code
+
+            if sc_paste == 200:
+                print(G + ' [ pasted ]' + W)
+                returned_json = rqst_paste.content.decode('utf-8', 'ignore')
+                output = json.loads(returned_json)
+                self.paste_dict[mail] = output
+            elif sc_paste == 404:
+                print(R + ' [ Not Pasted ]' + W)
+            else:
+                print('\n')
+                print(R + '[-]' + C + ' An Unknown Error Occurred')
+                print(rqst_paste.text)
+            time.sleep(1.5)  # For rate limit of haveibeenpwned API.
+            self.save_paste()
+
+    def save_all(self):
+        with open('breached_{}.json'.format(self.domain_name), 'w') as fp:
+            json.dump(self.breached_dict, fp, sort_keys=True, indent=4)
+
+    def save_pwned(self):
+        with open('short_{}.json'.format(self.domain_name), 'w') as fp:
+            json.dump(self.pwned_dict, fp, sort_keys=True, indent=4)
+
+    def save_paste(self):
+        with open('paste_{}.json'.format(self.domain_name), 'w') as fp:
+            json.dump(self.paste_dict, fp, sort_keys=True, indent=4)
+
+    def test(self):
+        with open('stm_hunter.txt', 'r') as fr:
+            for line in fr:
+                self.email_list.add(line.strip('\n'))
+        with open('test_snovio.txt', 'r') as rf:
+            for mail in rf:
+                self.email_list.add(mail.strip('\n'))
+
+
+if __name__ == '__main__':
+    # Accept domain name from user
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-d", "--domain", required=True, help="domain name to look for")
+    args = ap.parse_args()
+
+    email_instance = EmailLeaks(args.domain, keys.HUNTER_KEY, keys.SNOV_CLIENT_ID, keys.SNOV_CLIENT_SECRET,
+                                keys.HIBP_API_KEY)
+    try:
+        email_instance.domain_search_hunter()
+        email_instance.domain_search_snovio()
+        email_instance.check_breached_email()
+        email_instance.check_pwned_paste()
+    except Exception as e:
+        print(str(e))
+        with open(args.domain + ".txt", "w", encoding="UTF-8") as fp:
+            fp.write("\n".join(email_instance.email_list))
